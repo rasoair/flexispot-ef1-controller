6801b68198b43cc8fd7052000541be53bef38bde
rasoair <5x.deltabase@gmail.com>
2024-06-07 21:25:17 +0900

    [fix] 昇降ボタンを押した際に画面が遷移してしまう問題を解決

--- src/app.py
+++ src/app.py
@@ -82,7 +82,8 @@ def index():
 @app.route("/command", methods=["POST"])
 def command():
     try:
-        command = request.form.get('command')
+        req_data = request.get_json()
+        command = req_data['command']
         locktek = create_loctek_instance()
         locktek.execute_command(command)
         return jsonify({"status": "success", "command": command}), 200
